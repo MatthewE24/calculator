303eea687e19c13f236555f1ee0734b4681bda7e
MatthewE24 <46689658+MatthewE24@users.noreply.github.com>
2019-01-14 12:30:00 -0800

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,25 @@
+# global variables
+
+param1=0
+param2=0
+op ="+"
+# function definition
+def intro():
+    print("Welcome to THE calculator")
+    return
+
+def inputBoi():
+    param1 = input("what is your first parameter?")
+    param2 = input("what is your second parameter?")
+    op = input("what is your operator?")
+    return
+
+def calculate():
+    
+    
+    
+    
+    
+#execution code
+intro()
+inputBoi()

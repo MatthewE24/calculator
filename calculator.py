7ea91c636ad196bd9faeee97aab27377edd1119f
MatthewE24 <46689658+MatthewE24@users.noreply.github.com>
2019-01-28 12:28:35 -0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,23 +3,46 @@
 param1=0
 param2=0
 op ="+"
+x=1
+
 # function definition
 def intro():
     print("Welcome to THE calculator")
     return
 
 def inputBoi():
+    global param1
+    global param2
+    global op
     param1 = input("what is your first parameter?")
     param2 = input("what is your second parameter?")
     op = input("what is your operator?")
     return
 
 def calculate():
-    
-    
-    
+    global op
+    if(op=="+"):
+       print(int(param1)+int(param2))
+    elif(op=="-"):
+       print(int(param1)-int(param2))
+    elif(op=="*"):
+       print(int(param1)*int(param2))
+    elif(op=="/"):
+       print(int(param1)/int(param2))
+    return
+
+def end():
+    global x
+    y=input("would you like to continue")
+    if(y=="yes"):
+        x=1
+    else:
+        return
     
     
 #execution code
 intro()
-inputBoi()
+while(x==1):
+    inputBoi()
+    calculate()
+    end()
